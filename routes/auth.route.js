96b1d2909b5958806bcb3e85da4156a56378b853
Ritzsizl <ritzraha4u@gmail.com>
2024-12-22 21:36:05 +0530

    signIn api route creation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../api/controllers/auth.controllers.js';
+import { signin, signup } from '../api/controllers/auth.controllers.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signup);
+authRouter.post('/signin', signin);
 
 export default authRouter;
