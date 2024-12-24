0dff1a92be21d02f78a3e21d34c5cd73b8964d26
Ritzsizl <ritzraha4u@gmail.com>
2024-12-24 22:44:56 +0530

    update user functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from '../routes/user.route.js';
 import authRouter from '../routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() => {
@@ -11,7 +12,10 @@ mongoose.connect(process.env.MONGODB).then(() => {
     console.log("Error connecting to DB")
 })
 const app = express();
+
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { test } from '../api/controllers/user.controllers.js';
-
+import { updateUser } from '../api/controllers/user.controllers.js';
+import { verifyToken } from '../api/utils/verifyUser.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/test', test);
+userRouter.post('/update/:id', verifyToken,updateUser);
 
 export default userRouter;
